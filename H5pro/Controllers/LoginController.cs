79c502cbd6a0f31de2296c2cc20d6464f1bc0569
jjkruuse <36202901+jjkruuse@users.noreply.github.com>
2018-09-28 08:55:25 +0200

    Login

--- H5pro/Controllers/LoginController.cs
+++ H5pro/Controllers/LoginController.cs
@@ -13,7 +13,36 @@ namespace H5pro.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DataClassDataContext db = new DataClassDataContext())
+                {
+                    var obj = db.Users.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.Id.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("UserProfil");
+                    }
+                }
+            }
+            return View(objUser);
+        }
 
-
+        public ActionResult UserProfil()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
     }
 }
