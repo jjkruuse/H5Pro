53dafb9fb7b5eb5ca0c4a7c3ad42d9e90e6c9c78
MTeuber <32986982+MTeuber@users.noreply.github.com>
2018-10-03 13:23:31 +0200

    Encryption on  user creation and login

--- H5pro/Controllers/CreateUserController.cs
+++ H5pro/Controllers/CreateUserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography;
 
 namespace H5pro.Controllers
 {
@@ -17,6 +18,21 @@ namespace H5pro.Controllers
         [HttpPost]
         public ActionResult CreateUser(User user)
         {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(user.PasswordHash, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+            user.PasswordHash = savedPasswordHash;
+
+            string salted = Convert.ToBase64String(salt);
+            user.Salt = salted;
             if(ModelState.IsValid)
             {
                 DataClassDataContext db = new DataClassDataContext();
--- H5pro/Controllers/LoginController.cs
+++ H5pro/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Security.Cryptography;
 
 namespace H5pro.Controllers
 {
@@ -20,14 +21,35 @@ namespace H5pro.Controllers
         {
             if (ModelState.IsValid)
             {
+
                 using (DataClassDataContext db = new DataClassDataContext())
                 {
-                    var obj = db.Users.Where(a => a.Username.Equals(objUser.Username) && a.PasswordHash.Equals(objUser.PasswordHash)).FirstOrDefault();
+                    var obj = db.Users.Where(a => a.Username.Equals(objUser.Username)).FirstOrDefault();
+
                     if (obj != null)
                     {
-                        Session["UserID"] = obj.UserID.ToString();
-                        Session["UserName"] = obj.Username.ToString();
-                        return RedirectToAction("Profil","User");
+                        Boolean auth = true;
+                        string savedPassHash = obj.PasswordHash;
+                        byte[] hashBytes = Convert.FromBase64String(savedPassHash);
+                        byte[] salt = new byte[16];
+                        Array.Copy(hashBytes, 0, salt, 0, 16);
+                        var pbkdf2 = new Rfc2898DeriveBytes(objUser.PasswordHash, salt, 10000);
+                        byte[] hash = pbkdf2.GetBytes(20);
+
+                        for (int i = 0; i < 20; i++)
+                        {
+                            if (hashBytes[i+16] != hash[i])
+                            {
+                                auth = false;
+                            }
+                        }
+
+                        if (auth == true)
+                        {
+                            Session["UserID"] = obj.UserID.ToString();
+                            Session["UserName"] = obj.Username.ToString();
+                            return RedirectToAction("Profil", "User");
+                        }
                     }
                 }
             }
