21a0b0d07fd54c31af4a2871a272e0fe8783d0c6
jjkruuse <36202901+jjkruuse@users.noreply.github.com>
2018-10-04 08:40:19 +0200

    fejl

--- H5pro/Controllers/MessagesController.cs
+++ H5pro/Controllers/MessagesController.cs
@@ -8,9 +8,36 @@ namespace H5pro.Controllers
 {
     public class MessagesController : Controller
     {
+        DataClassDataContext db = new DataClassDataContext();
+
         // GET: Messages
         public ActionResult Messages()
         {
+            List<Message> messages = db.Messages.ToList();
+
+            return View(messages);
+        }
+
+
+
+
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message message)
+        {
+            if(ModelState.IsValid)
+            {
+               
+
+                db.Messages.InsertOnSubmit(message);
+                db.Messages.Context.SubmitChanges();
+
+                return View();
+            }
             return View();
         }
     }
